3a16cc592466a09119e33f82ae6096ce48345fdc
Alex <133618614+digithub1@users.noreply.github.com>
2026-09-14 00:47:04 +0530

    Neutralize adSense.js

--- adSense.js
+++ adSense.js
@@ -1,7 +1,1 @@
-(function() {
-    let script = document.createElement("script");
-    script.async = true;
-    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7321073664976914";
-    script.crossOrigin = "anonymous";
-    document.head.appendChild(script);
-})();
+/* ads removed */ window.adsbygoogle = window.adsbygoogle || []; window.adsbygoogle.push = function(){};
