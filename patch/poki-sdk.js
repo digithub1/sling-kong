046d880c9b97c0a1e5fbf1e8ab7b0e4b6eec3d17
Alex <133618614+digithub1@users.noreply.github.com>
2026-09-14 00:51:29 +0530

    Upgrade PokiSDK mock with Proxy and gameLoading hooks

--- patch/poki-sdk.js
+++ patch/poki-sdk.js
@@ -1,4 +1,4 @@
-window.PokiSDK = {
+window.PokiSDK = new Proxy({
     init: function() { return Promise.resolve(true); },
     commercialBreak: function() { return Promise.resolve(true); },
     rewardedBreak: function() { return Promise.resolve(true); },
@@ -10,6 +10,14 @@ window.PokiSDK = {
     happyHour: function() {},
     roundStart: function() {},
     roundEnd: function() {},
-    customEvent: function() {}
-};
+    customEvent: function() {},
+    gameLoadingStart: function() {},
+    gameLoadingProgress: function() {},
+    gameLoadingFinished: function() {}
+}, {
+    get: function(target, prop) {
+        if (prop in target) return target[prop];
+        return function() { return Promise.resolve(true); };
+    }
+});
 if (typeof pokiSDKLoaded === "function") pokiSDKLoaded();
